8e0db7cff08c2b98b9972f37b45de8fb8a176bad
QH <xskilldp@gmail.com>
2021-02-23 14:23:35 +0200

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a6929f168eb9e9395b757d6a681a3574'),
+    },
+  },
+});
